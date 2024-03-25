0ad9c8412697bb3a446b2adbcaa9d64b84b7f887
Milind Jamnekar <milindjamnekar502@gmail.com>
2024-03-25 20:41:44 +0530

    fuzzie setup + simple navbar

---
+++ components/global/Navbar.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {};
+
+const Navbar = async (props: Props) => {
+  return (
+    <header
+      className="fixed top-1 left-0 right-0 p-4
+     bg-black/40 backdrop-blur-lg z-[100] 
+     flex items-center border-b-[1px] border-neutral-900 justify-between"
+    >
+      <aside className="flex items-center gap-[2px]">
+        <p className="font-bold text-3xl">Fu</p>
+        <Image
+          src="/fuzzieLogo.png"
+          width={15}
+          height={15}
+          alt="Fuzzie logo"
+          className="shodow-sm"
+        />
+        <p className="font-bold text-3xl">zie</p>
+      </aside>
+
+      <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+        <ul className="flex items-center gap-4 list-none">
+          <li>
+            <Link href="#">Product</Link>
+          </li>
+          <li>
+            <Link href="#">Pricing</Link>
+          </li>
+          <li>
+            <Link href="#">Clients</Link>
+          </li>
+          <li>
+            <Link href="#">Resources</Link>
+          </li>
+          <li>
+            <Link href="#">Documentation</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <aside className="flex items-center gap-4">
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            {true ? "Dashboard" : "Get Started"}
+          </span>
+        </Link>
+        {/* {user ? <UserButton afterSignOutUrl="/" /> : null} */}
+        {/* <MenuIcon className="md:hidden" /> */}
+      </aside>
+    </header>
+  );
+};
+
+export default Navbar;
