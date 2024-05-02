47073fb9add1e5162d0bff2a264dfc390d9d2119
Milind Jamnekar <milindjamnekar502@gmail.com>
2024-05-02 21:54:39 +0530

    all working again

--- app/api/auth/callback/notion/route.ts
+++ app/api/auth/callback/notion/route.ts
@@ -4,67 +4,48 @@ import { Client } from "@notionhq/client";
 
 export async function GET(req: NextRequest) {
   const code = req.nextUrl.searchParams.get("code");
-
-  const url = process.env.URL || "http://localhost:3000";
-
-  const redirectUrl = `${url}/api/auth/callback/notion`;
-
   const encoded = Buffer.from(
     `${process.env.NOTION_CLIENT_ID}:${process.env.NOTION_API_SECRET}`
   ).toString("base64");
   if (code) {
-    try {
-      const response = await axios("https://api.notion.com/v1/oauth/token", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-          Authorization: `Basic ${encoded}`,
-          "Notion-Version": "2022-06-28",
-        },
-        data: JSON.stringify({
-          grant_type: "authorization_code",
-          code: code,
-          redirect_uri: redirectUrl,
-        }),
+    const response = await axios("https://api.notion.com/v1/oauth/token", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Basic ${encoded}`,
+        "Notion-Version": "2022-06-28",
+      },
+      data: JSON.stringify({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: process.env.NOTION_REDIRECT_URI!,
+      }),
+    });
+    if (response) {
+      const notion = new Client({
+        auth: response.data.access_token,
       });
-      if (response) {
-        const notion = new Client({
-          auth: response.data.access_token,
-        });
-        const databasesPages = await notion.search({
-          filter: {
-            value: "database",
-            property: "object",
-          },
-          sort: {
-            direction: "ascending",
-            timestamp: "last_edited_time",
-          },
-        });
-        const databaseId = databasesPages?.results?.length
-          ? databasesPages.results[0].id
-          : "";
+      const databasesPages = await notion.search({
+        filter: {
+          value: "database",
+          property: "object",
+        },
+        sort: {
+          direction: "ascending",
+          timestamp: "last_edited_time",
+        },
+      });
+      const databaseId = databasesPages?.results?.length
+        ? databasesPages.results[0].id
+        : "";
 
-        return NextResponse.redirect(
-          `${url}/connections?access_token=${response.data.access_token}&workspace_name=${response.data.workspace_name}&workspace_icon=${response.data.workspace_icon}&workspace_id=${response.data.workspace_id}&database_id=${databaseId}`
-        );
-      }
-    } catch (error) {
-      return NextResponse.json(
-        { message: "Error from making request to noton api" },
-        {
-          status: 500,
-        }
+      console.log(databaseId);
+
+      return NextResponse.redirect(
+        `http://localhost:3000/connections?access_token=${response.data.access_token}&workspace_name=${response.data.workspace_name}&workspace_icon=${response.data.workspace_icon}&workspace_id=${response.data.workspace_id}&database_id=${databaseId}`
       );
     }
-  } else {
-    return NextResponse.json(
-      { message: "Error because code is not there" },
-      {
-        status: 404,
-      }
-    );
   }
 
-  return NextResponse.redirect(`${url}/connections`);
+  return NextResponse.redirect("https://localhost:3000/connections");
 }
