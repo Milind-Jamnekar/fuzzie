57e754b00ac2838cc0e6271dda9e4f86e0b8e7f5
Milind Jamnekar <milindjamnekar502@gmail.com>
2024-05-15 20:12:44 +0530

    logo change

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -16,77 +16,13 @@ import { usePathname } from "next/navigation";
 const MenuOptions = () => {
   const pathName = usePathname();
 
-  // return (
-  //   <nav className=" dark:bg-black h-screen overflow-scroll justify-between flex items-center flex-col gap-10 py-6 px-2">
-  //     <div className="flex items-center justify-center flex-col gap-8">
-  //       <Link className="flex font-bold flex-row " href="/">
-  //         fuzzie.
-  //       </Link>
-  //       <TooltipProvider>
-  //         {menuOptions.map((menuItem) => (
-  //           <ul key={menuItem.name}>
-  //             <Tooltip delayDuration={0}>
-  //               <TooltipTrigger>
-  //                 <li>
-  //                   <Link
-  //                     href={menuItem.href}
-  //                     className={clsx(
-  //                       "group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer",
-  //                       {
-  //                         "dark:bg-[#2F006B] bg-[#EEE0FF] ":
-  //                           pathName === menuItem.href,
-  //                       }
-  //                     )}
-  //                   >
-  //                     <menuItem.Component
-  //                       selected={pathName === menuItem.href}
-  //                     />
-  //                   </Link>
-  //                 </li>
-  //               </TooltipTrigger>
-  //               <TooltipContent
-  //                 sideOffset={10}
-  //                 side="right"
-  //                 className="bg-white/50 dark:bg-black/50 backdrop-blur-xl"
-  //               >
-  //                 <p>{menuItem.name}</p>
-  //               </TooltipContent>
-  //             </Tooltip>
-  //           </ul>
-  //         ))}
-  //       </TooltipProvider>
-  //       <Separator />
-  //       <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-full overflow-scroll border-[1px]">
-  //         <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
-  //           <LucideMousePointerClick className="dark:text-white" size={18} />
-  //           <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
-  //         </div>
-  //         <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
-  //           <GitBranch className="text-muted-foreground" size={18} />
-  //           <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
-  //         </div>
-  //         <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
-  //           <Database className="text-muted-foreground" size={18} />
-  //           <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
-  //         </div>
-  //         <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
-  //           <GitBranch className="text-muted-foreground" size={18} />
-  //         </div>
-  //       </div>
-  //     </div>
-  //     <div className="flex items-center justify-center flex-col gap-8">
-  //       <ModeToggle />
-  //     </div>
-  //   </nav>
-  // );
-
   return (
     <>
       <aside className="inset-y-0 left-0 z-10 hidden w-14 flex-col bg-background sm:flex px-2">
         <TooltipProvider>
           <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
             <Link
-              className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+              className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-slate-200 text-lg font-semibold text-foreground dark:text-primary-foreground dark:bg-primary md:h-8 md:w-8 md:text-base"
               href="/"
             >
               fu
